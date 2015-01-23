27c960561e123c5f8d3c13efb1513d02e77e3d86
Raghu Betina <rbetina@github.com>
2015-01-23 09:35:42 -0600

    Generated Role resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Role resource:
+  # CREATE
+  get '/roles/new',      :controller => 'roles', :action => 'new',    :as => 'new_role'
+  post '/roles',         :controller => 'roles', :action => 'create', :as => 'roles'
+
+  # READ
+  get '/roles',          :controller => 'roles', :action => 'index'
+  get '/roles/:id',      :controller => 'roles', :action => 'show',   :as => 'role'
+
+  # UPDATE
+  get '/roles/:id/edit', :controller => 'roles', :action => 'edit',   :as => 'edit_role'
+  patch '/roles/:id',    :controller => 'roles', :action => 'update'
+
+  # DELETE
+  delete '/roles/:id',   :controller => 'roles', :action => 'destroy'
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
